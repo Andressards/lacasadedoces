af13ebf6840f7ea0f2718c5d14dd91cd5a7b7884
Andressa <andressarodrigues.est@gmail.com>
2025-02-09 17:55:32 -0300

    form de produto estilizado

---
+++ catalogo-produtos/resources/views/layouts/main.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>LaCasaDeDoces</title>
+
+    <!-- Fonte do Google -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+
+    <!-- CSS Boostrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- CSS da aplicação -->
+    <link rel="stylesheet" href="/css/style.css">
+
+    <!-- Script do Ionicons -->
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</head>
+<body>
+
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="collapse navbar-collapse" id="navbar">
+            <a href="/" class="navbar-brand">
+                <img src="/img/logo_lacasadedoces-removebg-preview.png" alt="lacasadedoces">
+            </a>
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a href="/consultas/dashboard_relatorio" class="nav-link">Produtos</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/consultas/dashboard_relatorio" class="nav-link">Pedidos Abertos</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/consultas/grid_cadastro_membro" class="nav-link">Cadastrar Produtos</a>
+                </li>
+                <li class="nav-item">
+                    <form action="/logout" method="POST">
+                        @csrf
+                        <a href="/logout" class="nav-link"
+                           onclick="event.preventDefault();
+                           this.closest('form').submit();">Sair</a>
+                    </form>
+                </li>
+            </ul>
+            <navbar-brand></navbar-brand>
+        </div>
+    </nav>
+</header>
+<main class="d-flex flex-column">
+    <div class="container-fluid flex-grow-1">
+        <div class="row">
+            @if(session('msg'))
+                <p class="msg">{{ session('msg') }}</p>
+            @endif
+            @yield('content')
+        </div>
+    </div>
+</main>
+
+<footer class="mt-auto">
+    <p>La Casa de Doces</p>
+</footer>
+
+</body>
+</html>
--- catalogo-produtos/resources/views/produtos/produtos_create.blade.php
+++ catalogo-produtos/resources/views/produtos/produtos_create.blade.php
@@ -1,20 +1,31 @@
-<div>
-    <form action="{{ route('produtos.store') }}" method="POST">
-    @csrf
-        <div class="form-group">
-            <label for="nome">Nome:</label>
-            <input type="text" class="form-control" id="nome-produto" name="nome" placeholder="Digite o nome" required>
+@extends('layouts.main')
+
+@section('content')
+
+    <div id="membro-create-container" class="create-container">
+        <div class="header-container">
+            <h1 class="titulo-form" id="titulo-form-membro">Registrar Produto</h1>
         </div>
-        <div class="form-group">
-            <label for="descricao">Descrição:</label>
-            <input type="text" class="form-control" id="descricao-produto" name="descricao" placeholder="Digite o descrição do produto" required>
-        </div>
-        <div class="form-group">
-            <label for="preco">Preço:</label>
-            <input type="number" class="form-control" id="preco-produto" name="preco" placeholder="Digite o preço do produto" required>
-        </div>
-        <div class="btn-container">
-            <input type="submit" class="btn btn-primary" value="Cadastrar">
-        </div>
-    </form>
-</div>
+
+        <div class="form">
+        <form action="{{ route('produtos.store') }}" method="POST">
+        @csrf
+            <div class="form-group">
+                <label for="nome">Nome:</label>
+                <input type="text" class="form-control" id="nome-produto" name="nome" placeholder="Digite o nome" required>
+            </div>
+            <div class="form-group">
+                <label for="descricao">Descrição:</label>
+                <input type="text" class="form-control" id="descricao-produto" name="descricao" placeholder="Digite o descrição do produto" required>
+            </div>
+            <div class="form-group">
+                <label for="preco">Preço:</label>
+                <input type="number" class="form-control" id="preco-produto" name="preco" placeholder="Digite o preço do produto" required>
+            </div>
+            <div class="btn-container">
+                <input type="submit" class="btn btn-primary" value="Cadastrar">
+            </div>
+        </form>
+    </div>
+
+@endsection
